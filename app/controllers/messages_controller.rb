26918c8e42ce865d9e1fe8fbd224ed91b3204eb7
doron rotem <doronrotem@gmail.com>
2014-03-09 18:55:31 +0200

    get messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,15 @@
 class MessagesController < ApplicationController
 
   def index
+    msgs = Message.all
+    if params['bookmark']
+      msgs = msgs.where('id > ?', params['bookmark'])
+    end
+    
+    respond_to do |format|
+      format.json { render json: msgs.to_json }
+    end
+    
   end
 
   def create
